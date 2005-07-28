ce18cb00b6f1586341f41b40c6d862cb5b86e19c
Juan C. Muller <jcmuller@cpan.org>
2005-07-28 16:22:23 +0000

    Import of JCMULLER/IO-LCDproc-0.03 from CPAN.
    gitpan-cpan-distribution: IO-LCDproc
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JCMULLER/IO-LCDproc-0.03.tar.gz
    gitpan-cpan-author:       JCMULLER
    gitpan-cpan-maturity:     released

--- LCDproc.pm
+++ LCDproc.pm
@@ -1,6 +1,52 @@
 use 5.008001;
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
+package IO::LCDproc;
+
+=head1 NAME
+
+	IO::LCDproc - Perl extension to connect to an LCDproc ready display.
+
+=head1 SYNOPSIS
+
+	use IO::LCDproc;
+
+	my $client	= IO::LCDproc::Client->new(name => "MYNAME");
+	my $screen	= IO::LCDproc::Screen->new(name => "screen", client => $client);
+	my $title 	= IO::LCDproc::Widget->new(
+			name => "date", type => "title", screen => $screen
+			);
+	my $first	= IO::LCDproc::Widget->new(
+			name => "first", align => "center", screen => $screen, xPos => 1, yPos => 2
+			);
+	my $second	= IO::LCDproc::Widget->new(
+			name => "second", align => "center", screen => $screen, xPos => 1, yPos => 3
+			);
+	my $third	= IO::LCDproc::Widget->new(
+			name => "third", screen => $screen, xPos => 1, yPos => 4
+			);
+	$client->add( $screen );
+	$screen->add( $title, $first, $second, $third );
+	$client->connect() or die "cannot connect: $!";
+	$client->initialize();
+
+	$title->set( data => "This is the title" );
+	$first->set( data => "First Line" );
+	$second->set( data => "Second line" );
+	$third->set( data => "Third Line" );
+
+=head1 DESCRIPTION
+
+	Follow the example above. Pretty straight forward. You create a client, assign a screen,
+	add widgets, and then set the widgets.
+
+=head2 IO::LCDproc::Client
+
+	It is the back engine of the module. It generates the connection to a ready listening server.
+
+=head3 METHODS
+
+=cut
 
 ####################################
 package IO::LCDproc::Client;
@@ -9,37 +55,72 @@ use Carp;
 use Fcntl;
 use IO::Socket::INET;
 
+=item new( name => 'Client_Name' [, host => $MYHOSTNAME] [, port => $MYPORTNUMBER] )
+
+	Constructor. Takes the following possible arguments (arguments must be given in key => value form):
+	C<host>, C<port>, and C<name>. C<name> is required.
+
+=cut
+
 sub new {
 	my $proto 		= shift;
 	my $class 		= ref($proto) || $proto;
 	my %params	 	= @_;
 	croak "No name for Client: $!" unless($params{name});
 	my $self  		= {};
-	$self->{name} 	= $params{name} if($params{name});
+	$self->{name} 	= $params{name};
+	$self->{host}	= $params{host} || "localhost";
+	$self->{port}	= $params{port} || "13666";
 	$self->{cmd}	= "client_set name {$self->{name}}\n";
 	$self->{screen}	= undef;
    bless ($self, $class);
    return $self;
 }
 
+=item add( I<SCREENREF> )
+
+	Adds the screen that will be attached to this client.
+
+=cut
+	
 sub add {
 	my $self = shift;
 	$self->{screen} = shift;
 }
 
+=item connect()
+
+	Establishes connection to LCDproc server (LCDd).
+
+=cut
+
 sub connect {
 	my $self = shift;
 	$self->{lcd}	= IO::Socket::INET->new(
-		Proto => "tcp", PeerAddr => "localhost", PeerPort => 13666
-	) or die "Cannot connect to LCDproc port: $!";
+		Proto => "tcp", PeerAddr => "$self->{host}", PeerPort => "$self->{port}"
+	) or croak "Cannot connect to LCDproc port: $!";
 	$self->{lcd}->autoflush();
 	sleep 1;
 }
 
+=item initialize()
+
+	Initializes client, screen and all the widgets  with the server.
+
+=cut
+
 sub initialize {
 	my $self = shift;
-	my $fh = *{$self->{lcd}};
+	my $fh = $self->{lcd};
+	my $msgs;
 	print $fh "hello\n";
+	$msgs = <$fh>;
+	if($msgs =~ /lcd.+wid\s+(\d+)\s+hgt\s+(\d+)/){
+		$self->{width}  = $1;
+		$self->{height} = $2;
+	} else {
+		croak "No stats reported...: $!";
+	}
 	fcntl( $fh, F_SETFL, O_NONBLOCK );
 
 	print $fh $self->{cmd};
@@ -55,22 +136,39 @@ package IO::LCDproc::Screen;
 
 use Carp;
 
+=head2 IO::LCDproc::Screen
+
+=head3 METHODS
+
+=item new( name => 'MYNAME', client => $CLIENTREF )
+
+	Constructor. Allowed options:
+	C<heartbeat>.
+	
+=cut
+
 sub new {
 	my $proto			= shift;
 	my $class			= ref($proto) || $proto;
 	my %params			= @_;
 	croak "No name for Screen: $!" unless($params{name});
-	my $self			= {};
-	$self->{client}		= $params{client} || die "No Client: $!";
-	$self->{name}		= $params{name} if($params{name});
-	$self->{heartbeat}	= $params{heartbeat} || "on";
+	my $self				= {};
+	$self->{client}	= $params{client} || croak "No Client: $!";
+	$self->{name}		= $params{name};
+	$self->{heartbeat}= $params{heartbeat} || "on";
 	$self->{cmd}		= "screen_add $self->{name}\n";
 	$self->{set}		= "screen_set $self->{name} name {$self->{client}->{name}}\n";
-	$self->{set}	   .= "screen_set $self->{name} heartbeat $self->{heartbeat}\n";
+	$self->{set}	  .= "screen_set $self->{name} heartbeat $self->{heartbeat}\n";
 	$self->{widgets}	= undef;
 	bless ($self, $class);
 	return $self;
 }
+
+=item add( @WIDGETS )
+	
+	Adds the given widgets to this screen.
+
+=cut
 
 sub add {
 	my $self = shift;
@@ -84,116 +182,96 @@ package IO::LCDproc::Widget;
 
 use Carp;
 
+=head2 IO::LCDproc::Widget
+
+=head3 METHODS
+
+=item new( name => 'MYNAME', screen => $SCREENREF )
+
+	Constructor. Allowed arguments:
+	C<align>, C<type> (string, title, vbar, hbar, ...), C<xPos>, C<yPos>, C<data>
+
+=cut
+
 sub new {
 	my $proto		= shift;
 	my $class		= ref($proto) || $proto;
 	my %params		= @_;
 	croak "No name for Widget: $!" unless($params{name});
 	my $self			= {};
-	$self->{screen}	= $params{screen} || die "No Screen: $!";
-	$self->{name}	= $params{name} if($params{name});
+	$self->{screen}	= $params{screen} || croak "No Screen: $!";
+	$self->{name}	= $params{name};
 	$self->{align}	= $params{align} || "left";
 	$self->{type}	= $params{type}  || "string";
-	$self->{xPos}	= $params{xPos} || "";
-	$self->{yPos}	= $params{yPos} || "";
+	$self->{xPos}	= $params{xPos}  || "";
+	$self->{yPos}	= $params{yPos}  || "";
+	$self->{data}	= $params{data} if( $params{data} );
 	$self->{cmd}	= "widget_add $self->{screen}->{name} $self->{name} $self->{type}\n";
 	bless ($self, $class);
 	return $self;
 }
 
+=item set()
+
+	Sets the widget to the spec'd args. They may be given on the function call or the may be
+	pre specified.
+	C<xPos>, C<yPos>, C<data>
+
+=cut
+
 sub set {
 	my $self = shift;
-	my $fh = *{$self->{screen}->{client}->{lcd}};
+	my %params = @_;
+	$self->{xPos} = $params{xPos} if($params{xPos});
+	$self->{yPos} = $params{yPos} if($params{yPos});
+	$self->{data} = $params{data} if($params{data});
+	my $fh = $self->{screen}->{client}->{lcd};
 	print $fh "widget_set $self->{screen}->{name} $self->{name} $self->{xPos} $self->{yPos} {" .
-		($self->{align} =~ /center/ ? $self->center($_[0]) : $_[0]) . "}\n";
+		($self->{align} =~ /center/ ? $self->_center($self->{data}) : $self->{data}) . "}\n";
 }
 
-sub center {
+sub _center {
 	my $self = shift;
-	return ( " " x (10 - ( length ( $_[0] ) / 2) ) . $_[0]);
+	return ( " " x ( ($self->{screen}->{client}->{width} - length ( $_[0] ) ) / 2 ) . $_[0]);
+}
+
+=item save()
+
+	Saves current data to be user later.
+
+=cut
+
+sub save {
+	my $self = shift;
+	$self->{saved} = $self->{data};
+}
+
+=item restore()
+
+	Restore previously saved data.
+
+=cut
+
+sub restore {
+	my $self = shift;
+	$self->{data} = $self->{saved};
 }
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
-
-=head1 NAME
-
-IO::LCDproc - Perl extension to connect to an LCDproc ready display.
-
-=head1 SYNOPSIS
-
-  use IO::LCDproc;
-
-  my $client	= IO::LCDproc::Client->new(name => "MYNAME");
-  my $screen	= IO::LCDproc::Screen->new(name => "screen", client => $client);
-  my $title 	= IO::LCDproc::Widget->new(
-    name => "date", type => "title", screen => $screen
-  );
-  my $first	= IO::LCDproc::Widget->new(
-    name => "first", align => "center", screen => $screen, xPos => 1, yPos => 2
-  );
-  my $second	= IO::LCDproc::Widget->new(
-    name => "second", align => "center", screen => $screen, xPos => 1, yPos => 3
-  );
-  my $third	= IO::LCDproc::Widget->new(
-    name => "third", screen => $screen, xPos => 1, yPos => 4
-  );
-  $client->add( $screen );
-  $screen->add( $title, $first, $second, $third );
-  $client->connect() or die "cannot connect: $!";
-  $client->initialize();
-
-  $title->set("This is the title");
-  $first->set("First Line");
-  $second->set("Second line");
-  $third->set("Third Line");
-
-
-=head1 DESCRIPTION
-
-Follow the example above. Pretty straight forward. You create a client, assign a screen,
-add widgets, and then set the widgets.
-
-=over 
-
-=head2 IO::LCDproc::Client
-
-It is the back engine of the module. It generates the connection to a ready listening server.
-
-=back
-
-=over
-
-=head2 IO::LCDproc::Screen
-
-This is the middleman between the Client and the Widgets.
-
-=back
-
-=over
-
-=head2 IO::LCDproc::Widget
-
-These are what we create to send info to the server.
-
-=back
-
-=head1 Methods
-
 
 
 =head1 SEE ALSO
 
-  LCDd L<>
+  L<LCDd>
 
 =head1 AUTHOR
 
-Juan C. Müller, E<lt>sputnik@nonetE<gt>
+Juan C. Muller, E<lt>jcmuller@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005 by Juan C. Müller
+Copyright 2005 by Juan C. Muller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
